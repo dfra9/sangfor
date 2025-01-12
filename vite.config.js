8abc8c80e36414ede58d2453f8d269fec56d7698
DefaraPutra9 <defara41@gmail.com>
2025-01-12 17:49:41 +0700

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: "resources/js/app.tsx",
             refresh: true,
         }),
+        react(),
     ],
-    build: {
-        outDir: "public/build", // This is where Laravel expects the built files
-        manifest: true,
-        input: {
-            app: '/path/to/your/app.js'  // Update this path
-    },
 });
